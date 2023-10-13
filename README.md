8aaaf53188c97054fe1b757d1795e8f68ecf65c3
Portland.HODL <git@qrsnap.io>
2023-10-13 15:27:30 -0700

    added features to README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Nyan Switch
+# Nyan Keys
+
+An FPGA based mechanical keybaord controller design based around the Lattice ice40hx series of FPGA chips.
+
+## Why?
+
+The biggest question to be asked is why? The answer is simple. A quest to create the most performant keyboard
+on planet earth. The additional performance is extracted through four means.
+
+    - Per key programmable debounce counters (2 bit)
+    - Per key parallel input _All keys are parallel without a scanning matrix_
+    - Ultra fast SPI based serialization
+    - Lowest possible latency from key to computer
+    - nKRO - Though standard now it's nice to have becuase of the parallel interface
+
+## FPGA IP
+
+FPGA IP is designed to be build using Yosys and Nextpnr. 
+
+## STM32 USB HID Interface
--- rtl/vendor/spi.v
+++ rtl/vendor/spi.v
@@ -20,30 +20,34 @@ module mojo_spi_slave(
     reg [7:0] dout_d, dout_q;
     reg miso_d, miso_q;
 
+    // Output name aliasing
     assign miso = miso_q;
     assign done = done_q;
     assign dout = dout_q;
 
+    /**
+     * Cobo logic signal processing
+     */
     always @(*) begin
-        ss_d = ss;
-        mosi_d = mosi;
-        miso_d = miso_q;
-        sck_d = sck;
-        sck_old_d = sck_q;
-        data_d = data_q;
-        done_d = 1'b0;
-        bit_ct_d = bit_ct_q;
-        dout_d = dout_q;
+        ss_d      = ss;       // Slave Select input
+        mosi_d    = mosi;     // Master out Slave in input
+        miso_d    = miso_q;   // Master in Slave out output
+        sck_d     = sck;      // Slave clock input
+        sck_old_d = sck_q;    // Slave clock edge detection clk -> sck_q -> sck_d
+        data_d    = data_q;   
+        done_d    = 1'b0;
+        bit_ct_d  = bit_ct_q;
+        dout_d    = dout_q;
 
-        if (ss_q) begin                         // if slave select is high (deselected)
-            bit_ct_d = 3'b0;                    // reset bit counter
-            data_d = din;                       // read in data
-            miso_d = data_q[7];                 // output MSB
-        end else begin                          // else slave select is low (selected)
-            if (!sck_old_q && sck_q) begin      // rising edge
-                data_d = {data_q[6:0], mosi_q}; // read data in and shift
-                bit_ct_d = bit_ct_q + 1'b1;     // increment the bit counter
-                if (bit_ct_q == 3'b111) begin   // if we are on the last bit
+        if (ss_q) begin                             // if slave select is high (deselected)
+            bit_ct_d = 3'b0;                        // reset bit counter
+            data_d = din;                           // read in data
+            miso_d = data_q[7];                     // output MSB
+        end else begin                              // else slave select is low (selected)
+            if (!sck_old_q && sck_q) begin          // rising edge
+                data_d = {data_q[6:0], mosi_q};     // read data in and shift
+                bit_ct_d = bit_ct_q + 1'b1;         // increment the bit counter
+                if (bit_ct_q == 3'b111) begin       // if we are on the last bit
                     dout_d = {data_q[6:0], mosi_q}; // output the byte
                     done_d = 1'b1;                  // set transfer done flag
                     data_d = din;                   // immediately load the next byte
@@ -55,23 +59,27 @@ module mojo_spi_slave(
         end
     end
 
+    /**
+     * This block takes the outputs of the combo logic aboves 
+     * and latches them to the output signals
+     */
     always @(posedge clk) begin
         if (!rst) begin
-            done_q <= 1'b0;
+            done_q   <= 1'b0;
             bit_ct_q <= 3'b0;
-            dout_q <= 8'b0;
-            miso_q <= 1'b1;
+            dout_q   <= 8'b0;
+            miso_q   <= 1'b1;
         end else begin
-            done_q <= done_d;
+            done_q   <= done_d;
             bit_ct_q <= bit_ct_d;
-            dout_q <= dout_d;
-            miso_q <= miso_d;
+            dout_q   <= dout_d;
+            miso_q   <= miso_d;
         end
 
-        sck_q <= sck_d;
-        mosi_q <= mosi_d;
-        ss_q <= ss_d;
-        data_q <= data_d;
+        sck_q     <= sck_d;
+        mosi_q    <= mosi_d;
+        ss_q      <= ss_d;
+        data_q    <= data_d;
         sck_old_q <= sck_old_d;
     end
 
