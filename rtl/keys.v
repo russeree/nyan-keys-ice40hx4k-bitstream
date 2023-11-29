69dd4acc0927fe4ac93cc4cc07ae3f3610893572
Portland.HODL <git@qrsnap.io>
2023-11-29 02:17:38 -0800

    simplified debounce logic

--- rtl/keys.v
+++ rtl/keys.v
@@ -1,49 +1,28 @@
 module keys #(parameter keys = 61) (
-    input             clk_i,
-    input             rst_n_i,
-    input  [keys-1:0] keys_i,
-    output [keys-1:0] keys_o
+    input                  clk_i,
+    input  wire            rst_n_i,
+    input  wire [keys-1:0] keys_i,
+    output reg  [keys-1:0] keys_o
     );
 
     // each key is represented by a 7-bit register
     reg [7:0] counter   [keys-1:0];
-    reg [0:0] direction [keys-1:0];
 
     integer key;
 
-    // debouncing counters
+    // Debouncing Counter - Arm after pin change and counter high
     always @(posedge clk_i) begin
-        // unroll the key inputs into the counters
         for (key = 0; key < keys; key = key + 1) begin
             if (rst_n_i == 1'b0) begin
                 counter[key] <= 1'b0;
-            end else if (keys_i[key] == 1'b1 && counter[key] != 8'hFF) begin
+                keys_o[key]  <= 1'b1;
+            end else if (counter[key] != 8'hFF) begin
                 counter[key] <= counter[key] + 1'b1;
-            end else if (counter[key] != 8'h00) begin
-                counter[key] <= counter[key] - 1'b1;
+            end else if (keys_o[key] != keys_i[key]) begin
+                keys_o[key] <= keys_i[key];
+                counter[key] <= 8'h00;
             end
         end
     end
-
-    // keystate logic
-    always @(posedge clk_i) begin
-        for (key = 0; key < keys; key = key + 1) begin
-            if (rst_n_i == 1'b0) begin
-                direction[key] <= 1'b1;
-            end else if (counter[key] == 8'h00 && direction[key] == 1'b1) begin
-                direction[key] <= 1'b0;
-            end else if (counter[key] == 8'hFF && direction[key] == 1'b0) begin
-                direction[key] <= 1'b1;
-            end
-        end
-    end
-
-    // trace the register outputs to their module outputs
-    generate
-        genvar trace;
-        for(trace = 0; trace < keys; trace = trace + 1) begin
-            assign keys_o[trace] = direction[trace];
-        end
-    endgenerate
 
 endmodule
